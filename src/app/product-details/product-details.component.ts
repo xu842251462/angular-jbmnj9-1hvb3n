2eef2b8f62517010aa06495e69c373e6e7764be3
xu842251462 <xu842251462@gmail.com>
2019-11-24 16:18:56 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,7 +17,10 @@ export class ProductDetailsComponent implements OnInit {
     private cartService: CartService
   ) {}
 
-  
+   addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
